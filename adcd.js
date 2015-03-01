f1ae9a9ecb6e0a94ccb01c359276f09d7cfca029
ramanauday1561 <ramanauday1561@gmail.com>
2015-03-02 01:22:46 +0530

    MyResume

--- adcd.js
+++
@@ -1,5 +0,0 @@
-var myApp = angular.module('simpleModule',[]);
-
-myApp.controller('MyController',['$scope',function ($scope){
-	$scope.title = 'Fucking Change';
-}]);
--- mymodule.js
+++
@@ -1,5 +0,0 @@
-var myApp = angular.module('myModule',[]);
-
-myApp.controller('sayHello',['$scope',function ($scope){
-	$scope.name = {text: 'You Name' };
-}]);
